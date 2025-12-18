5339603e615a0e82b1674e8c6d1ce6950f05cc1d
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-18 19:08:23 +0100

    Download official `tar` instead of compiling it

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,17 +4,36 @@ set -eu
 
 ARCH=$(uname -m)
 
-echo "Installing package dependencies..."
+echo "Downloading and extracting Tutanota tar file..."
 echo "---------------------------------------------------------------"
-make-aur-package tutanota-desktop
+
+TAR_LINK=$(curl -s https://api.github.com/repos/tutao/tutanota/releases | jq -r '.[] | select(.name | contains("(Desktop)")) | .assets[] | select(.name | endswith(".tar.gz")) | .browser_download_url' | head -n 1)
+
+if ! wget --retry-connrefused --tries=30 "$TAR_LINK" -O /tmp/tuta.tar.gz 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
+
+tar -xvf /tmp/data.tar.xz
+rm -f /tmp/tuta.tar.gz
+
+mkdir -p ./AppDir/bin
+cp -rv /tmp/linux-unpacked/* ./AppDir/bin/
+cp -v /tmp/linux-unpacked/resources/icons/icon/512.png ./AppDir/.DirIcon
+
+cat << 'EOF' > ./AppDir/tutanota-desktop.desktop
+[Desktop Entry]
+Name=Tuta Mail
+Exec=tutanota-desktop
+Terminal=false
+Type=Application
+Icon=tutanota-desktop
+StartupWMClass=tutanota-desktop
+Comment=The desktop client for Tutanota, the secure e-mail service.
+MimeType=x-scheme-handler/mailto;
+Categories=Network;
+EOF
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
-
-mkdir -p ./AppDir/bin
-cp -rv /opt/tutanota-desktop/*                                        ./AppDir/bin
-cp -v  /usr/share/applications/tutanota-desktop.desktop               ./AppDir
-cp -v  /usr/share/icons/hicolor/512x512/apps/tutanota-desktop.png     ./AppDir/.DirIcon
-
-sed -i 's|^Exec=.*|Exec=tutanota-desktop|' ./AppDir/tutanota-desktop.desktop
